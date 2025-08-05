1bfb5c1bff97ad1c716968ad88801daf2bc97795
peppotto89 <peppotto89@gmail.com>
2025-08-05 12:48:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = FastAPI()
 @app.get("/checkk")
 async def check_buttons():
     return JSONResponse(content={
-            "status": "ohohohohoo6666666",
-            "error": "error erroe"
+            "status": "ohohohohoo777777",
+            "prova": "prova"
         }, status_code=500)
         
 @app.get("/check")
@@ -20,20 +20,20 @@ async def check_buttons():
             context = await browser.new_context()
             page = await context.new_page()
             await page.goto("https://www.popmart.com/it/products/5610", timeout=30000)
-            await page.wait_for_timeout(5000)  # aspetta caricamento
+            await page.wait_for_timeout(15000)  # aspetta caricamento
 
             await page.wait_for_selector("div.index_euBtn__7NmZ6.index_red__kx6Ql", timeout=15000)
 
             # Cerca bottone BUY NOW per testo (case insensitive)
-            buy_now_count = await page.locator("text=/BUY NOW/i").count()
+            #buy_now_count = await page.locator("text=/BUY NOW/i").count()
             # Cerca bottone ADD TO CART per testo (case insensitive)
-            add_to_cart_count = await page.locator("text=/ADD TO CART/i").count()
+            #add_to_cart_count = await page.locator("text=/ADD TO CART/i").count()
+            found = await page.locator("text=/your connection before proceeding/i").count()
 
             await browser.close()
 
             return JSONResponse(content={
-                "buy_now_found": buy_now_count > 0,
-                "add_to_cart_found": add_to_cart_count > 0,
+                "security": found > 0,
                 "status": "success"
             })
 
